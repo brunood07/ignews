ee91a5cc474872317ba736fce64d05a668e36eaf
Bruno Domingues <brunood07@gmail.com>
2022-01-01 16:36:33 -0300

    autenticação inserida utilizando o nextauth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,17 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
       Bruno Domingues
@@ -19,6 +21,7 @@ export function SignInButton() {
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn("github")}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
