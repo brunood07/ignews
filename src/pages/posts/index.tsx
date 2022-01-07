8901194b0435c55f7c80a22de0655fb864e8ccca
Bruno Domingues <brunood07@gmail.com>
2022-01-07 14:43:22 -0300

    página de posts agora utiliza os dados recebidos do prismic e formatados

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,8 +1,24 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
+
+import { getPrismicClient } from "../../services/prismic";
 
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -11,23 +27,45 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>04 de Janeiro de 2022</time>
-            <strong>Creating a Monorepo</strong>
-            <p>In this guide, you will learn</p>
-          </a>
-          <a href="#">
-            <time>04 de Janeiro de 2022</time>
-            <strong>Creating a Monorepo</strong>
-            <p>In this guide, you will learn</p>
-          </a>
-          <a href="#">
-            <time>04 de Janeiro de 2022</time>
-            <strong>Creating a Monorepo</strong>
-            <p>In this guide, you will learn</p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query<any>([
+    Prismic.Predicates.at("document.type", "publication")
+  ], {
+    fetch: ["publication.title", "publication.content"],
+    pageSize: 100,
+  });
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === "paragraph")?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric"
+      })
+    };
+  });
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
