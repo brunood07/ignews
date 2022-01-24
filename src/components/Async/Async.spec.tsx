669a0ab6c0334bd9cb45dd26c3de960dad1d2257
Bruno Domingues <brunood07@gmail.com>
2022-01-24 00:26:16 -0300

    testes concluidos

---
+++ src/components/Async/Async.spec.tsx
@@ -0,0 +1,17 @@
+import { render, screen, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
+
+import { Async } from ".";
+
+describe("Async component", () => {
+  it("should renders correctly", async () => {
+    render(<Async />);
+
+    expect(screen.getByText("Hello World")).toBeInTheDocument();  
+    
+    // await waitForElementToBeRemoved(screen.queryByText("Button"));
+
+    await waitFor(() => {
+      return expect(screen.getByText("Button")).toBeInTheDocument();
+    });
+  });
+});
